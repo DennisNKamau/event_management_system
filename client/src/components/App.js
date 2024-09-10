000115c6c67ed4354089b7ea706462e2d0bae2ad
Dennis Njau <njaudennis30@gmail.com>
2024-09-10 18:34:48 +0300

    update components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,32 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import GuestHomePage from "../pages/GuestHomePage";
+import UserHomePage from "../pages/UserHomePage";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import EventsPage from "../pages/EventsPage";
+import Event from "../pages/Event";
+import CreateEvent from "../pages/CreateEvent";
+import Contact from "../pages/Contact";
+import { UserProvider } from "../pages/context/UserContext";
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+    <Router>
+      <UserProvider>
+        <Switch>
+          <Route exact path="/" component={GuestHomePage} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/user" component={UserHomePage} />
+          <Route exact path="/events" component={EventsPage} />
+          <Route path="/events/:id" component={Event} />
+          <Route path="/event-form" component={CreateEvent} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </UserProvider>  
+    </Router>
+  );
 }
 
 export default App;
